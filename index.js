36eb2619cadc654a8201e4e81fce1b6673156b9c
zDragonSK <skstudios.contato@gmail.com>
2023-10-14 11:47:56 -0300

    Update a comment in index

--- index.js
+++ index.js
@@ -5,6 +5,7 @@
 const {Client, Events, GatewayIntentBits} = require('discord.js');
 const dotenv = require('dotenv').config();
 
+// Build a new client
 const client = new Client({intents:[
   GatewayIntentBits.Guilds,
   GatewayIntentBits.GuildMembers
