b5202952f45ab2fdc5cf027e02d404e9902c50d0
zDragonSK <skstudios.contato@gmail.com>
2023-10-14 00:03:01 -0300

    update base

---
+++ index.js
@@ -0,0 +1,17 @@
+/*
+    Servant - New Version
+      ~ BY: .gg/cyberz
+*/
+const {Client, Events, GatewayIntentBits} = require('discord.js');
+const dotenv = require('dotenv').config();
+
+const client = new Client({intents:[
+  GatewayIntentBits.Guilds,
+  GatewayIntentBits.GuildMembers
+]});
+
+client.once(Events.ClientReady, c => {
+  console.log(`${c.user.tag} is ready!`);
+});
+
+client.login(process.env.token);
