911a70156dd3ac22b29e5bc59ee464cd1f709f62
Gabriel Rosa <gabr1sr@proton.me>
2023-10-16 17:08:26 -0300

    simplify command handler
    Command handler code reduced and glob changed to `src/commands/*/*.js`

--- src/handlers/commandHandler.js
+++ src/handlers/commandHandler.js
@@ -1,42 +1,25 @@
 const { glob } = require('fast-glob');
 const { REST, Routes } = require('discord.js');
-require('dotenv').config();
 const { dirname } = require('path');
+
 const appDir = dirname(require.main.filename);
 
-const rest = new REST({ version: 10 }).setToken(process.env.token);
-
-const COMMANDS_GLOB = 'src/commands/**/*.js';
-
-const CLIENT_ID = process.env.CLIENT_ID;
+const COMMANDS_GLOB = 'src/commands/*/*.js';
 
 const commands = [];
 
 const assocs = {};
 
-const initApplicationCommands = async (options) => {
-  await loadGlobalCommands(options);
-
-  const files = await glob(COMMANDS_GLOB);
-
-  files.forEach((file) => {
-    const req = require(appDir + '/' + file);
-    commands.push(req.metadata);
-    assocs[req.metadata.name] = req.run;
-    if (options.debug) console.debug(`Loaded command ${req.metadata.name}`);
+const initApplicationCommands = async (options = {}) => {
+  (await glob(COMMANDS_GLOB)).forEach((file) => {
+    const command = require(`${appDir}/${file}`);
+    commands.push(command.metadata);
+    assocs[command.metadata.name] = command.run;
   });
 
-  await manageCommands(options);
-};
-
-const loadGlobalCommands = async (options) => {
-  const data = await rest.get(Routes.applicationCommands(CLIENT_ID));
-  if (options.debug) console.debug(data);
-};
-
-const manageCommands = async (options) => {
-  const data = await rest.put(Routes.applicationCommands(CLIENT_ID), { body: commands });
-  if (options.debug) console.debug(data);
+  const clientID = process.env.CLIENT_ID;
+  const rest = new REST({ version: 10 }).setToken(process.env.token);
+  await rest.put(Routes.applicationCommands(clientID), { body: commands });
 };
 
 const handleCommand = async (interaction) => {
