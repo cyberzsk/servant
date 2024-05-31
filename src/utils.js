2c3568007f02f70b957cb869c7f13fa05157f20c
Gabriel Rosa <gabr1sr@proton.me>
2024-05-31 15:57:43 -0300

    chore: use concise character class syntax '\d' in utils.js regex

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,5 @@
-const userMentionRegExp = /<@!*([0-9]+)>/;
-const snowflakeRegExp = /([0-9]+)/;
+const userMentionRegExp = /<@!*(\d+)>/;
+const snowflakeRegExp = /(\d+)/;
 
 const memberIdsFromString = (members) => {
   return members.split(' ').flatMap((member) => userMentionRegExp.exec(member)?.at(1) ?? snowflakeRegExp.exec(member)?.at(1) ?? []);
